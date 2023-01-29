16b4d38c8b109cf461328550dfea197628b64680
Centauri Soldier <3165890+CentauriSoldier@users.noreply.github.com>
2023-01-28 19:25:46 -0800

    LuaEx Update

--- CoG/test.lua
+++ CoG/test.lua
@@ -38,7 +38,71 @@ local k = bullet({id = "34sdf"});
 --k.id = null
 --k.id = "asd3234asd"
 --k.id = 4567
-p(type(k))
+--p(type(k))
 --p(struct.struct.bullet)
 --p(type(bullet))
 --p(type(struct))
+--constant("ZERO", 0)
+--local sub = enum("SUB", {"ONE", "TWO"}, true);
+--[[enum("TEST", {"SUB1", "SUB2"}, {enum("SUB1", {"ONE", "TWO", "THREE", "FOUR", "FIVE"}, nil, true),
+								enum("SUB2", {"SIX", "SEVEN", "EIGHT", "NINE", "TEN"}, nil, true)
+							});
+
+t = {
+	k = TEST.SUB1.TWO,
+	r = TEST.SUB2.SEVEN,
+}
+
+enum("COLOR", {"RED", "GREEN", "BLUE"})
+--st = serialize.table(t)
+--print(12)
+--print(type(TEST.SUB1.ONE.FIV));
+local tSub1Meta = getmetatable(TEST.SUB1.TWO.parent);
+local nMetaCount = 0;
+
+for sIndex, vValue in TEST() do
+	print(vValue)
+end
+
+--print(nMetaCount, serialize.table(tSub1Meta));
+--print(type(TEST.SUB1()))
+--print(TEST.SUB.valueType)
+--print(st)
+--print(type(TEST))
+--print(COLOR.RED.enum.__name)
+
+--check if item is of type enum.
+]]
+
+enum("NUMBER", {"ODD", "EVEN"}, {
+				enum("ODD", 	{"ONE", "THREE", "FIVE", "SEVEN", "sd"},		nil, true),
+				enum("EVEN", 	{"TWO", "FOUR", "SIX", "EIGHT", "TEN"}, nil, true),
+				})
+--COLOR.RED.OR = 0
+local k = NUMBER.EVEN.TWO
+print(NUMBER[1].next())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--sd
